3c5f12dbd9eec97ae120ad7d6f46d50b1aa402b4
ofaasd <ofaasd@gmail.com>
2024-03-15 06:40:51 +0700

    update menu sesuai excel

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -20,64 +20,212 @@
                     <span><i class="fa fa-book"></i> Master Data</span>
                 </a>
                 <ul class="sidebar-submenu">
-                    <li><a href="{{ route('ruang')}}">Ruang</a></li>
+                    <li>
+                        <a class="submenu-title" href="#">
+                            <span>PT (Perguruan Tinggi)</span>
+                        </a>
+                        <ul class="nav-sub-childmenu submenu-content">
+                            <li><a href="#">Profile</a></li>
+                            <li><a href="#">Atribut</a></li>
+                            <li><a href="#">Renstra</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a class="submenu-title" href="#">
+                            <span>PRODI</span>
+                        </a>
+                        <ul class="nav-sub-childmenu submenu-content">
+                            <li><a href="#">Daftar Prodi</a></li>
+                            <li><a href="#">Atribut Prodi</a></li>
+                            <li><a href="#">Akreditasi</a></li>
+                            <li><a href="#">Renstra</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Tahun Ajaran</a></li>
+                    <li><a href="#">Jenjang</a></li>
+                    <li><a href="#">Jabatan</a></li>
+                    <li>
+                        <a class="submenu-title" href="#">
+                            <span>User</span>
+                        </a>
+                        <ul class="nav-sub-childmenu submenu-content">
+                            <li><a href="#">Manajemen</a></li>
+                            <li><a href="#">Hak Akses</a></li>
+                        </ul>
+                    </li>
+                    {{-- <li><a href="{{ route('ruang/')}}">Ruang</a></li> --}}
+                    <li><a href="#">Sesi</a></li>
+                    <li><a href="#">Sekolah</a></li>
                     <li><a href="#">Waktu</a></li>
                     <li><a href="#">Rumpun</a></li>
                     <li><a href="#">Fakultas</a></li>
-                    <li><a href="#">Program Studi</a></li>
-                    <li><a href="#">Matakuliah</a></li>
-                    <li><a href="#">Kurikulum</a></li>
-                    <li><a href="#">Rombel</a></li>
-                </ul>
-            </li>
-
-            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#" >
-                <span><i class="fa fa-users"></i> Kepegawaian</span></a>
-                <ul class="sidebar-submenu">
-                    <li><a href="#">Data Pegawai</a></li>
-                    <li><a href="#">Profil Pegawai</a></li>
-                    <li><a href="#">Lihat KRM</a></li>
-                    <li><a href="#">Struktur</a></li>
-                </ul>
-            </li>
-            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#" >
-               <span><i class="fa fa-university"></i> Akademik</span></a>
-               <ul class="sidebar-submenu">
-                    <li><a href="#">Data Mahasiswa</a></li>
-                    <li><a href="#">Pengaturan NIM</a></li>
-                    <li><a href="#">Jadwal</a></li>
-                    <li><a href="#">KRS</a></li>  {{--  Ada menu untuk toggle krs sedang dibuka atau ditutup--}}
-                    <li><a href="#">Perwalian</a></li>
-                    <li><a href="#">Setting Pertemuan</a></li>
-                    <li><a href="#">Presensi</a></li>
-                    <li><a href="#">Nilai</a></li>
-                    <li><a href="#">Pengaturan Ujian</a></li>
-                    <li><a href="#">Reset Password</a></li>
-                    <li><a href="#">KHS</a></li>
-                    <li><a href="#">Yudisium</a></li>
-                    <li><a href="#">Transkrip / Ijazah</a></li>
                 </ul>
             </li>
             <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#" >
                 <span><i class="fa fa-file-text"></i> Admisi</span></a>
                 <ul class="sidebar-submenu">
-                    <li><a href="#">Pendaftaran Mahasiswa</a></li>
-                    <li><a href="#">Surat Pengumuman</a></li>
-                    <li><a href="#">Statistik</a></li>
-                    <li><a href="#">Peringkat PMDP</a></li>  {{--  Ada menu untuk toggle krs sedang dibuka atau ditutup--}}
                     <li><a href="#">Gelombang</a></li>
-                    <li><a href="#">USPI</a></li>
-                    <li><a href="#">Pembayaran</a></li>
-                    <li><a href="#">Daftar Sekolah</a></li>
-                    <li><a href="#">Verifikasi PMB Online</a></li>
-                    <li><a href="#">Daftar PMB Online</a></li>
+                    <li><a href="#">Pendaftaran Mahasiswa</a></li>
                     <li><a href="#">Pengaturan Ujian PMB</a></li>
+                    <li><a href="#">Surat Pengumuman</a></li>
+                    <li><a href="#">Peringkat PMDP</a></li>  {{--  Ada menu untuk toggle krs sedang dibuka atau ditutup--}}
+                    <li><a href="#">Daftar PMB Online</a></li> {{-- Daftar User pmb online--}}
+                    <li><a href="#">KTM (s)</a></li>
+                    <li><a href="#">Statistik</a></li>
+                    <li><a href="#">Verifikasi Pendaftaran</a></li>
+                    <li><a href="#">Verifikasi Pembayaran Daftar</a></li>
+                    <li><a href="#">Verifikasi Pembayaran Registrasi</a></li>
+                    <li><a href="#">History PMB</a></li>
+                    <li><a href="#">Berita Pendaftaran</a></li>
+                    <li><a href="#">Berita Gambar/Slideshow</a></li>
                 </ul>
             </li>
+            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#" >
+                <span><i class="fa fa-university"></i> Akademik</span></a>
+                <ul class="sidebar-submenu">
+                     <li><a href="#">Matakuliah</a></li>
+                     <li><a href="#">Jadwal</a></li>
+                     <li><a href="#">Plot Jadwal Ajar</a></li>
+                     <li><a href="#">Kontrol Jadwal</a></li>
+                     <li>
+                        <a class="submenu-title" href="#">
+                            <span>KRS</span>  {{--  Ada menu untuk toggle krs sedang dibuka atau ditutup--}}
+                        </a>
+                        <ul class="nav-sub-childmenu submenu-content">
+                            <li><a href="#">Input KRS</a></li>
+                            <li><a href="#">Statistik KRS</a></li>
+                            <li><a href="#">Monitoring KRS</a></li>
+                        </ul>
+                    </li>
+                     <li><a href="#">Semester Antara</a></li>
+                     <li><a href="#">Remidial</a></li>
+                     <li><a href="#">KHS</a></li>
+                     <li>
+                        <a class="submenu-title" href="#">
+                            <span>Ujian</span>  
+                        </a>
+                        <ul class="nav-sub-childmenu submenu-content">
+                            <li><a href="#">Pengaturan</a></li>{{--  Ada menu untuk toggle ujian sedang dibuka atau ditutup--}}
+                            <li><a href="#">Input Nilai</a></li>
+                            <li><a href="#">Posting Nilai</a></li>
+                            <li><a href="#">Input Nilai Konversi</a></li>
+                            <li><a href="#">Komplain Nilai / Susulan</a></li>
+                        </ul>
+                    </li>
+                     <li><a href="#">Perubahan Status Akademik</a></li>
+                     <li><a href="#">Setting Pertemuan</a></li>
+                     <li><a href="#">Perwalian</a></li>
+                     
+                     <li>
+                        <a class="submenu-title" href="#">
+                            <span>Presensi</span>  
+                        </a>
+                        <ul class="nav-sub-childmenu submenu-content">
+                            <li><a href="#">Input Presensi</a></li>
+                            <li><a href="#">Ganti Kuliah - Pengganti</a></li>
+                            
+                        </ul>
+                    </li>
+                    <li>
+                        <a class="submenu-title" href="#">
+                            <span>Yuidisium</span>  
+                        </a>
+                        <ul class="nav-sub-childmenu submenu-content">
+                            <li><a href="#">Setting</a></li>
+                            <li><a href="#">Proses</a></li>
+                            <li><a href="#">Cetak</a></li>
+                            
+                        </ul>
+                    </li>
+                     <li><a href="#">Persuratan</a></li>
+                     <li>
+                        <a class="submenu-title" href="#">
+                            <span>Transkrip / Ijazah</span>  
+                        </a>
+                        <ul class="nav-sub-childmenu submenu-content">
+                            <li><a href="#">Print Ijazah</a></li>
+                            <li><a href="#">Print Transkrip</a></li>
+                            <li><a href="#">Legalisir</a></li>
+                            
+                        </ul>
+                    </li>
+                    <li>
+                        <a class="submenu-title" href="#">
+                            <span>Skripsi</span>  
+                        </a>
+                        <ul class="nav-sub-childmenu submenu-content">
+                            <li><a href="#">Manajemen Skripsi</a></li>
+                            <li><a href="#">Input Dosbing</a></li>
+                            <li><a href="#">Jadwal Sidang</a></li>
+                            <li><a href="#">Nilai</a></li>
+                        </ul>
+                    </li>
+                     <li><a href="#">Evaluasi Studi</a></li>
+                     <li><a href="#">Histori Jadwal</a></li>
+                     <li>
+                        <a class="submenu-title" href="#">
+                            <span>Cetak Berkas</span>  
+                        </a>
+                        <ul class="nav-sub-childmenu submenu-content">
+                            <li><a href="#">Absensi</a></li>
+                            <li><a href="#">Ujian</a></li>
+                        </ul>
+                    </li>
+                 </ul>
+             </li>
+            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#" >
+                <span><i class="fa fa-users"></i> Kepegawaian</span></a>
+                <ul class="sidebar-submenu">
+                    <li><a href="#">Data Pegawai</a></li>
+                    <li><a href="#">Profil Pegawai</a></li>
+                    <li><a href="#">Absensi</a></li>
+                    <li><a href="#">Jam Kerja Dosen</a></li>
+                    <li><a href="#">Keterlambatan</a></li>
+                    <li><a href="#">Statistik</a></li>
+                    <li><a href="#">Struktur</a></li>
+                    <li>
+                        <a class="submenu-title" href="#">
+                            <span>Riwayat</span>  
+                        </a>
+                        <ul class="nav-sub-childmenu submenu-content">
+                            <li><a href="#">Mengajar</a></li>
+                            <li><a href="#">Penelitian</a></li>
+                            <li><a href="#">Pengabdian</a></li>
+                            <li><a href="#">karya Ilmiah</a></li>
+                            <li><a href="#">Organisasi</a></li>
+                            <li><a href="#">Repositori</a></li>
+                            <li><a href="#">Jabatan Struktural</a></li>
+                            <li><a href="#">Jabatan Fungsional</a></li>
+                            <li><a href="#">Pekerjaan</a></li>
+                            <li><a href="#">Pendidikan</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Surat Izin</a></li>
+                </ul>
+            </li>
+            
+            
             <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#">
                 <span><i class="fa fa-money"></i> Keuangan</span></a>
                 <ul class="sidebar-submenu">
-                    <li><a href="#">Verifikasi Keuangan Mahasiswa</a></li>
+                    <li><a href="#">Setting SKS - UKT</a></li> {{-- Untuk setting pembayaran sks dalam bentuk paket --}}
+                    <li><a href="#">Input Laporan Pembayaran</a></li>
+                    <li><a href="#">Buat Tagihan</a></li> {{--Buat dan Publish Tagihan --}}
+                    <li><a href="#">Statistik</a></li>
+                    <li><a href="#">Cetak</a></li>
+                    <li><a href="#">Sync</a></li>
+                    <li><a href="#">Pembayaran Lain-lain</a></li>
+                </ul>
+            </li>
+            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#">
+                <span><i class="fa fa-support"></i> Aset</span></a>
+                <ul class="sidebar-submenu">
+                    <li><a href="#">Ruang</a></li>
+                    <li><a href="#">Kendaraan</a></li>
+                    <li><a href="#">Tanah & Bangunan</a></li>
+                    <li><a href="#">Elektronik</a></li>
+                    <li><a href="#">Pengajuan</a></li>
+                    <li><a href="#">Monev</a></li>
                 </ul>
             </li>
             <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#">
