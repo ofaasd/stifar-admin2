b87f262ee4c482be607cbfee49f7526be92e503c
ofaasd <ofaasd@gmail.com>
2024-08-15 11:23:43 +0700

    update dashboard mahasiswa

--- resources/views/layouts/sidebar_mhs.blade.php
+++ resources/views/layouts/sidebar_mhs.blade.php
@@ -4,7 +4,7 @@
         <div class="back-btn"><i class="fa fa-angle-left"></i></div>
         <div class="toggle-sidebar"><i class="status_toggle middle sidebar-toggle" data-feather="grid"> </i></div>
       </div>
-      <div class="logo-icon-wrapper"><a href="{{ route('dashboard')}}"><img class="img-fluid" src="{{ asset('assets/images/logo/logo-icon.png') }}" alt=""></a></div>
+      <div class="logo-icon-wrapper"><a href="{{ route('dashboard_mahasiswa')}}"><img class="img-fluid" src="{{ asset('assets/images/logo/logo-icon.png') }}" alt=""></a></div>
       <nav class="sidebar-main">
         <div class="left-arrow" id="left-arrow"><i data-feather="arrow-left"></i></div>
         <div id="sidebar-menu">
@@ -12,7 +12,7 @@
             <li class="back-btn">
               <div class="mobile-back text-end"><span>Back</span><i class="fa fa-angle-right ps-2" aria-hidden="true"></i></div>
             </li>
-            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="{{ route('dashboard')}}" >
+            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="{{ route('dashboard_mahasiswa')}}" >
                 <span><i class="fa fa-home"></i> Dashboard</span></a>
             </li>
             <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="{{URL::to('mhs/profile')}}" >
