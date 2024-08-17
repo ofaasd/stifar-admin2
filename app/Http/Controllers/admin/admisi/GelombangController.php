15cbf7e1da24a34b24945a6fd1907ba01c0c6cf1
ofaasd <ofaasd@gmail.com>
2024-08-17 22:49:07 +0700

    update gelombang model in gelombang controller

--- app/Http/Controllers/admin/admisi/GelombangController.php
+++ app/Http/Controllers/admin/admisi/GelombangController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\admin\admisi;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Gelombang;
+use App\Models\gelombang;
 use App\Models\PmbJalur as Jalur;
 
 class GelombangController extends Controller
