c128cf3542fb534d38957e4126abb38a93baeb26
ofaasd <ofaasd@gmail.com>
2024-09-11 14:55:35 +0700

    update foto pegawai on top right

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -40,7 +40,12 @@
           </div>
         </li>
         <li class="profile-nav onhover-dropdown pe-0 py-0">
-          <div class="media profile-media"><img class="b-r-10" src="{{ asset('assets/images/dashboard/profile.png') }}" alt="">
+          @php
+            if(Auth::user()->roles->pluck('name')[0] == 'pegawai'){
+                $pegawai = DB::table('pegawai_biodata')->where('user_id',Auth::user()->id)->first();
+            }
+          @endphp
+          <div class="media profile-media"><img class="b-r-10" src="{{ (!empty($pegawai->foto))?asset('assets/images/pegawai/' . $pegawai->foto):asset('assets/images/dashboard/profile.png') }}" alt="">
             <div class="media-body"><span>{{Auth::user()->name}}</span>
               <p class="mb-0 font-roboto">{{Auth::user()->roles->pluck('name')[0]}} <i class="middle fa fa-angle-down"></i></p>
             </div>
