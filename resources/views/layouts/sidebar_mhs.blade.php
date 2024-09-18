37143b74bb36899641b4b8ee3b69552b219741e2
ofaasd <ofaasd@gmail.com>
2024-09-18 11:50:30 +0700

    update dashboard link

--- resources/views/layouts/sidebar_mhs.blade.php
+++ resources/views/layouts/sidebar_mhs.blade.php
@@ -1,6 +1,6 @@
 <div class="sidebar-wrapper" sidebar-layout="stroke-svg">
     <div>
-      <div class="logo-wrapper"><a href="{{ route('dashboard')}}"><img class="img-fluid for-light" src="{{ asset('assets/images/logo/logo.png') }}" alt=""><img class="img-fluid for-dark" src="{{ asset('assets/images/logo/logo_dark.png') }}" alt=""></a>
+      <div class="logo-wrapper"><a href="{{ route('dashboard_mahasiswa')}}"><img class="img-fluid for-light" src="{{ asset('assets/images/logo/logo.png') }}" alt=""><img class="img-fluid for-dark" src="{{ asset('assets/images/logo/logo_dark.png') }}" alt=""></a>
         <div class="back-btn"><i class="fa fa-angle-left"></i></div>
         <div class="toggle-sidebar"><i class="status_toggle middle sidebar-toggle" data-feather="grid"> </i></div>
       </div>
--- resources/views/layouts/sidebar_pegawai.blade.php
+++ resources/views/layouts/sidebar_pegawai.blade.php
@@ -1,10 +1,10 @@
 <div class="sidebar-wrapper" sidebar-layout="stroke-svg">
     <div>
-      <div class="logo-wrapper"><a href="{{ route('dashboard')}}"><img class="img-fluid for-light" src="{{ asset('assets/images/logo/logo.png') }}" alt=""><img class="img-fluid for-dark" src="{{ asset('assets/images/logo/logo_dark.png') }}" alt=""></a>
+      <div class="logo-wrapper"><a href="{{ route('dsn/dashboard')}}"><img class="img-fluid for-light" src="{{ asset('assets/images/logo/logo.png') }}" alt=""><img class="img-fluid for-dark" src="{{ asset('assets/images/logo/logo_dark.png') }}" alt=""></a>
         <div class="back-btn"><i class="fa fa-angle-left"></i></div>
         <div class="toggle-sidebar"><i class="status_toggle middle sidebar-toggle" data-feather="grid"> </i></div>
       </div>
-      <div class="logo-icon-wrapper"><a href="{{ route('dashboard')}}"></a></div>
+      <div class="logo-icon-wrapper"><a href="{{ route('dsn/dashboard')}}"></a></div>
       <nav class="sidebar-main">
         <div class="left-arrow" id="left-arrow"><i data-feather="arrow-left"></i></div>
         <div id="sidebar-menu">
