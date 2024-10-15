35d12cdb1dc16e4d1bd425f73e50ff24afa57b16
ofaasd <ofaasd@gmail.com>
2024-10-15 21:08:55 +0700

    update pengajar model

--- app/Models/pengajar.php
+++ app/Models/pengajar.php
@@ -1,16 +1,36 @@
 <?php
 
+/**
+ * Created by Reliese Model.
+ */
+
 namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Pengajar
+ * 
+ * @property int $id
+ * @property int $id_jadwal
+ * @property int $id_dsn
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ *
+ * @package App\Models
+ */
 class Pengajar extends Model
 {
-    protected $fillable = [
-        'id_jadwal',
-        'id_dsn',
-    ];
+	protected $table = 'pengajars';
 
-    // hasmany ke simpeg dosen
+	protected $casts = [
+		'id_jadwal' => 'int',
+		'id_dsn' => 'int'
+	];
+
+	protected $fillable = [
+		'id_jadwal',
+		'id_dsn'
+	];
 }
