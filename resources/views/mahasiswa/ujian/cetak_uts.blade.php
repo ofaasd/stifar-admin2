0ac8c1cdf886914f501b9d961555bee8ef5fff5a
ofaasd <ofaasd@gmail.com>
2024-10-17 21:02:56 +0700

    update reisze foto cetak uts

--- resources/views/mahasiswa/ujian/cetak_uts.blade.php
+++ resources/views/mahasiswa/ujian/cetak_uts.blade.php
@@ -56,7 +56,7 @@
             <td>Semester</td>
             <td>:</td>
             <td>{{ $semester[$smt] }} - {{ $tahun_ajar }}</td>
-            <td rowspan="4" style="text-align: right"><img style="width:100px;" alt="" src="{{ (!empty($mhs->foto_mhs))?asset('assets/images/mahasiswa/' . $mhs->foto_mhs):asset('assets/images/user/7.jpg') }}"></td>
+            <td rowspan="4" style="text-align: right"><img style="width:100px;" alt="" src="{{ $foto }}"></td>
         </tr>
         <tr>
             <td>Jurusan/Prodi</td>
