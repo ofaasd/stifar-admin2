cd9c891d1d4e920b0b530aea137e93a2d6e7aa0f
ofaasd <ofaasd@gmail.com>
2025-02-17 07:12:52 +0700

    update arsip master nilai

---
+++ app/Models/MasterNilaiArsip.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MasterNilaiArsip extends Model
+{
+    use HasFactory;
+    protected $table = 'master_nilai_arsip';
+}
