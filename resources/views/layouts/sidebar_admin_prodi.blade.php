7a1e968abe9aa81139ffa399777f49d23f4fc6d8
Abdul Ghofar <ofaasd@gmail.com>
2025-07-23 09:04:52 +0700

    update sidebar admin prodi back to normal version

--- resources/views/layouts/sidebar_admin_prodi.blade.php
+++ resources/views/layouts/sidebar_admin_prodi.blade.php
@@ -12,50 +12,13 @@
             <li class="back-btn">
               <div class="mobile-back text-end"><span>Back</span><i class="fa fa-angle-right ps-2" aria-hidden="true"></i></div>
             </li>
-            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="{{ URL::to('dsn/dashboard')}}" >
+            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="{{ route('dashboard')}}" >
                 <span><i class="fa fa-home"></i> Dashboard</span></a>
             </li>
-            <li class="sidebar-list"><a class="sidebar-link sidebar-title" >
-                <span><i class="fa fa-users"></i> Profile Pegawai</span></a>
+            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#" >
+                <span><i class="fa fa-university"></i> Akademik</span></a>
                 <ul class="sidebar-submenu">
-                    <li>
-                        <a href="{{URL::to('pegawai')}}" class="submenu-title">Data Diri</a>
-                    </li>
-                    <li>
-                        <a class="submenu-title" href="{{URL::to('riwayat')}}" >
-                             Riwayat Pegawai</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="sidebar-list"><a class="sidebar-link sidebar-title" >
-                <span><i class="fa fa-bar-chart"></i> Akademik</span></a>
-                <ul class="sidebar-submenu">
-                    <li>
-                        <a class="submenu-title" href="{{URL::to('dosen/krm')}}" >
-                             KRM
-                        </a>
-                    </li>
-                    <li>
-                        <a class="submenu-title" href="{{URL::to('dosen/krm_riwayat')}}" >
-                            Riwayat KRM
-                        </a>
-                    </li>
-                    <li>
-                        <a class="submenu-title" href="{{URL::to('dosen/setting-pertemuan')}}" >
-                            Setting Pertemuan
-                        </a>
-                    </li>
-                    <li>
-                        <a class="submenu-title" href="{{URL::to('dosen/input_nilai')}}" >
-                             Input Nilai
-                        </a>
-                    </li>
-                    <li>
-                        <a class="submenu-title" href="#" >
-                             Daftar Nilai <label class="badge badge-light-danger">!</label>
-                        </a>
-                    </li>
-                    <li><a href="{{URL::to('admin/masterdata/matakuliah')}}">Matakuliah</a></li>
+                     <li><a href="{{URL::to('admin/masterdata/matakuliah')}}">Matakuliah</a></li>
                      <li><a href="{{URL::to('admin/masterdata/jadwal')}}">Jadwal</a></li>
                      {{-- <li><a href="#">Plot Jadwal Ajar</a></li> --}}
                      <li><a href="{{URL::to('admin/masterdata/jadwal-harian')}}">Kontrol Jadwal</a></li>
@@ -145,38 +108,8 @@
                             <li><a href="#">Ujian <label class="badge badge-light-danger">!</label></a></li>
                         </ul>
                     </li>
-                </ul>
-            </li>
-            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="{{URL::to('dosen/berkas')}}" >
-                <span><i class="fa fa-file"></i> Berkas</span></a>
-            </li>
-            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="{{URL::to('riwayat')}}" >
-                <span><i class="fa fa-users"></i> Riwayat Pegawai</span></a>
-            </li>
-            <li class="sidebar-list"><a class="sidebar-link sidebar-title" >
-                <span><i class="fa fa-book"></i> Bimbingan</span></a>
-                <ul class="sidebar-submenu">
-                    <li>
-                        <a class="submenu-title" href="#" >
-                             Konsultasi <label class="badge badge-light-danger">!</label>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="submenu-title" href="#" >
-                             Riwayat KRM <label class="badge badge-light-danger">!</label>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="{{route('Perwalian')}}" >
-                <span><i class="fa fa-bookmark"></i> Perwalian</span></a>
-            </li>
-            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#" >
-                <span><i class="fa fa-bookmark"></i> Kuesioner Kepuasan Dosen</span></a> <label class="badge badge-light-danger">!</label>
-            </li>
-            <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="{{URL::to('dsn/skripsi/pengajuan')}}" >
-                <span><i class="fa fa-users"></i> Pengajuan Mahasiswa</span></a>
-            </li>
+                 </ul>
+             </li>
 
           </ul>
         </div>
