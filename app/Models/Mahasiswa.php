152f5df37be0ed61a6c816acc79b96899b339860
Dyaka Faiz <dyakafaiz123@gmail.com>
2025-08-25 16:32:58 +0700

    update model mahasiswa

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Mahasiswa extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'mahasiswa';
     protected $fillable = [
         'nim', 'nama', 'no_ktp', 'jk', 'agama', 'tempat_lahir', 'tgl_lahir', 'nama_ibu', 'nama_ayah', 'hp_ortu', 'alamat', 'alamat_semarang', 'rt', 'rw', 'kelurahan', 'kecamatan', 'kokab', 'provinsi', 'telp', 'hp', 'email', 'paswd', 'status', 'foto_mhs', 'kelas', 'angkatan', 'ta', 'id_program_studi', 'id_dsn_wali','user_id','nopen', 'no_pisn', 'is_yudisium'
