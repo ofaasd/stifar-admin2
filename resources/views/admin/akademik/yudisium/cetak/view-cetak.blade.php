776b0915bf428397e260877cc4d0f4a156d64662
Dyaka Faiz <112964868+DyakaFaiz@users.noreply.github.com>
2025-11-03 01:40:41 +0700

    Update view-cetak.blade.php

--- resources/views/admin/akademik/yudisium/cetak/view-cetak.blade.php
+++ resources/views/admin/akademik/yudisium/cetak/view-cetak.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <title>{{ 'Yudisium-' . $gelombang->nama  }}</title>
+    {{-- <title>{{ 'Yudisium-' . $gelombang->nama  }}</title> --}}
     <style>
         body { font-family: 'Times New Roman', serif; font-size: 16px; line-height: 1.15; }
         .contai-portrait { width: 100%; }
