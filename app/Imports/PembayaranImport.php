7cfc939e133a266295a5f0b62b0c718027097760
Abdul Ghofar <ofaasd@gmail.com>
2026-02-09 10:07:40 +0700

    Update PembayaranImport.php

--- app/Imports/PembayaranImport.php
+++ app/Imports/PembayaranImport.php
@@ -4,7 +4,9 @@ namespace App\Imports;
 
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 use App\Models\TbPembayaran;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
 class PembayaranImport implements ToCollection
 {
@@ -15,12 +17,19 @@ class PembayaranImport implements ToCollection
     {
         //
         foreach ($rows as $row){
+            $tanggalBayar = $row[3] ?? null;
+            if ($tanggalBayar) {
+                $tanggalBayar = Date::excelToDateTimeObject($tanggalBayar)->format('Y-m-d');
+            } else {
+                $tanggalBayar = date('Y-m-d');
+            }
+            
             $pembayaran = TbPembayaran::create([
                 'nim' => $row[0],
                 'jumlah' => $row[1],
                 'keterangan' => $row[2] ?? '',
                 'status' => 1,
-                'tanggal_bayar' => $row[3] ?? date('Y-m-d'),
+                'tanggal_bayar' => $tanggalBayar,
             ]);
         }
     }
